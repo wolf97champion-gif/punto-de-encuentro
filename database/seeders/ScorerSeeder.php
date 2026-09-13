80aecf075f03973c0f797f1fe52ada998b236b52
wolf97champion-gif <wolf97champion@gmail.com>
2026-09-13 16:31:32 -0300

    Insertar goleadores directamente en la migración de la BD

--- database/seeders/ScorerSeeder.php
+++ database/seeders/ScorerSeeder.php
@@ -1,17 +1,27 @@
 <?php
 
-namespace Database\Seeders;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
-use Illuminate\Database\Seeder;
-use App\Models\Scorer;
-
-class ScorerSeeder extends Seeder
+return new class extends Migration
 {
-    public function run(): void
+    public function up(): void
     {
-        Scorer::truncate();
+        Schema::create('scorers', function (Blueprint $table) {
+            $table->id();
+            $table->integer('position')->nullable();
+            $table->string('name');
+            $table->string('nickname')->nullable();
+            $table->integer('matches');
+            $table->integer('goals');
+            $table->string('years');
+            $table->timestamps();
+        });
 
-        $scorers = [
+        // Carga automática de datos iniciales
+        DB::table('scorers')->insert([
             ['position' => 1, 'name' => 'Martín Palermo', 'nickname' => 'El Titán', 'matches' => 404, 'goals' => 236, 'years' => '1997-2011'],
             ['position' => 2, 'name' => 'Roberto Cherro', 'nickname' => 'Cabecita de Oro', 'matches' => 300, 'goals' => 218, 'years' => '1926-1938'],
             ['position' => 3, 'name' => 'Francisco Varallo', 'nickname' => 'Pancho', 'matches' => 222, 'goals' => 194, 'years' => '1931-1939'],
@@ -19,10 +29,11 @@ class ScorerSeeder extends Seeder
             ['position' => 5, 'name' => 'Jaime Sarlanga', 'nickname' => 'Piraña', 'matches' => 220, 'goals' => 129, 'years' => '1940-1948'],
             ['position' => 6, 'name' => 'Carlos Tevez', 'nickname' => 'El Apache', 'matches' => 279, 'goals' => 94, 'years' => '2001-2021'],
             ['position' => 7, 'name' => 'Juan Román Riquelme', 'nickname' => 'El Último 10', 'matches' => 388, 'goals' => 92, 'years' => '1996-2014'],
-        ];
-
-        foreach ($scorers as $scorer) {
-            Scorer::create($scorer);
-        }
+        ]);
     }
-}
+
+    public function down(): void
+    {
+        Schema::dropIfExists('scorers');
+    }
+};
